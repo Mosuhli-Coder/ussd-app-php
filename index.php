b655c9308606ba75c8d41f9d67f170b40467bdda
Mosuhli-Coder <drmosuhli@gmail.com>
2022-02-01 10:57:54 +0200

    update index file

--- index.php
+++ index.php
@@ -15,7 +15,26 @@ if ($text == ""){
     $response = "CON Choose account information you want to view \n";
     $response = "1. My Account Nnumber \n";
     $response = "2. Account Balance";
+} else if ($text == "2"){
+    // Business logic for the second response
+    // This is the terminal request> Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+} else if ($text == "1*1"){
+    // This is the second level response where the use selected 1 in the first instance of
+    $accountNumber = "ACC1001";
+
+    // This is a terminal request . Note how we start with END 
+    $response = "END Your account number is ".$accountNumber;
+} else if ($text == "1*2"){
+    // This is a second level response where the use selected 2 in the first instance
+    $balance = "KES 10 000";
+
+    // This is a terminal request. Note how we start with END 
+    $response = "END Your account number is ".$balance;
 }
+// echo the response to the API. The response depends on the statement that is fulfilled in each instance
+header('Content-type; text/plain');
+echo $response;
 
 
 ?>
